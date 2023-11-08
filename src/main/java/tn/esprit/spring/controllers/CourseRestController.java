bd26b57ed6c0f5ab4b1030b8995cefbc783f4072
feryel <feryel.bousselmi@esprit.tn>
2023-11-08 12:28:22 +0100

    update

--- src/main/java/tn/esprit/spring/controllers/CourseRestController.java
+++ src/main/java/tn/esprit/spring/controllers/CourseRestController.java
@@ -16,26 +16,30 @@ import java.util.List;
 public class CourseRestController {
     
     private final ICourseServices courseServices;
+    @java.lang.SuppressWarnings("squid:S2201")
 
     @Operation(description = "Add Course")
     @PostMapping("/add")
-    public Course addCourse(@RequestBody Course course){
-        return  courseServices.addCourse(course);
+    public Course addCourse(@RequestBody Course Course){
+        return  courseServices.addCourse(Course);
     }
 
     @Operation(description = "Retrieve all Courses")
+    @java.lang.SuppressWarnings("squid:S2201")
     @GetMapping("/all")
     public List<Course> getAllCourses(){
         return courseServices.retrieveAllCourses();
     }
 
     @Operation(description = "Update Course ")
+    @java.lang.SuppressWarnings("squid:S2201")
     @PutMapping("/update")
-    public Course updateCourse(@RequestBody Course course){
-        return  courseServices.updateCourse(course);
+    public Course updateCourse(@RequestBody Course Course){
+        return  courseServices.updateCourse(Course);
     }
 
     @Operation(description = "Retrieve Course by Id")
+    @java.lang.SuppressWarnings("squid:S2201")
     @GetMapping("/get/{id-course}")
     public Course getById(@PathVariable("id-course") Long numCourse){
         return courseServices.retrieveCourse(numCourse);
